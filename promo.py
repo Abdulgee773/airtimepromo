15c92eb8cc54b02aa1f125905239aff5a7517537
Abdulgee773 <abdullahihudu773@gmail.com>
2022-03-10 15:29:03 +0100

    promo

---
+++ promo.py
@@ -0,0 +1,33 @@
+import random
+import time
+import os
+os.system('cls' if os.name == 'nt' else 'clear')
+print("""                                  
+ _      _   _   ____  _  ____   __
+| |    | | | | / ___|| |/ /\ \ / /
+| |    | | | || |    | ' /  \ V / 
+| |___ | |_| || |___ | . \   | |  
+|_____| \___/  \____||_|\_\  |_|  
+                                  
+v1.8 by Abdulgee
+ 
+            GUINNESS PROMO
+
+""")
+
+g = ""
+print("1.start promo")
+g = input("select: ").lower()
+if g == "1":
+	time.sleep(2)
+	print("""
+	            get ready""")
+	time.sleep(4)
+	print("""
+guinness promo code is """, random.randint(111111111, 999999999))
+	print("""use *1759*Guinness promo code#""")
+	
+	
+print("""
+
+LUCKY GAME""")
